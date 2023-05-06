25c3f46348337468831e7f7a84ef40f71f4fbda8
mlhkcmn <melihkocaman06@hotmail.com>
2023-05-06 19:27:33 +0300

    backend added.

---
+++ routes/taskRouter.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const TaskCtrl = require('../controllers/taskCtrl');
+const auth = require('../middleware/auth');
+const authAdmin = require('../middleware/authAdmin')
+
+
+router.route('/task')
+    .get(TaskCtrl.getTasks)
+    .post(TaskCtrl.createTask)
+
+router.route('task/:id')
+    .delete(auth, TaskCtrl.deleteTask)
+    .put(auth, TaskCtrl.updateTask)
+
+module.exports = router
--- server/db/database.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv"
-import mongoose from "mongoose"
-
-dotenv.config()
-
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>console.log("connected to the database"))
-.catch(err=>console.log(err))
-
