22d2f63c49c6cfe172888878880b2db107e8a1b4
zcaymaz <zbyrcmyz@gmail.com>
2023-05-10 16:21:21 +0300

    gereksiz servisler pasife çekildi.

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
 
-const categorySchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true
-    }
-}, {
-    timestamps: true
-})
+// const categorySchema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         unique: true
+//     }
+// }, {
+//     timestamps: true
+// })
 
-module.exports = mongoose.model("Category", categorySchema)
+// module.exports = mongoose.model("Category", categorySchema)
--- models/productModel.js
+++ models/productModel.js
@@ -1,50 +1,50 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
 
-const productSchema = new mongoose.Schema({
-    product_id:{
-        type: String,
-        unique: true,
-        trim: true,
-        required: true
-    },
-    title:{
-        type: String,
-        trim: true,
-        required: true
-    },
-    address:{
-        type: String,
-        required: true
-    },
-    price:{
-        type: Number,
-        trim: true,
-        required: true
-    },
-    description:{
-        type: String,
-        required: true
-    },
-    type:{
-        type: String,
-        required: true
-    },
-    images:{
-        type: Object,
-        required: true
-    },
-    roomnumber:{
-        type: String,
-        required: true
-    },
-    features:{
-        type: String,
-        default: false
-    },
-}, {
-    timestamps: true //important
-})
+// const productSchema = new mongoose.Schema({
+//     product_id:{
+//         type: String,
+//         unique: true,
+//         trim: true,
+//         required: true
+//     },
+//     title:{
+//         type: String,
+//         trim: true,
+//         required: true
+//     },
+//     address:{
+//         type: String,
+//         required: true
+//     },
+//     price:{
+//         type: Number,
+//         trim: true,
+//         required: true
+//     },
+//     description:{
+//         type: String,
+//         required: true
+//     },
+//     type:{
+//         type: String,
+//         required: true
+//     },
+//     images:{
+//         type: Object,
+//         required: true
+//     },
+//     roomnumber:{
+//         type: String,
+//         required: true
+//     },
+//     features:{
+//         type: String,
+//         default: false
+//     },
+// }, {
+//     timestamps: true //important
+// })
 
 
-module.exports = mongoose.model("Products", productSchema)
+// module.exports = mongoose.model("Products", productSchema)
