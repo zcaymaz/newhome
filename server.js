7e566412f5a21d35fd12956c8455677e747720c3
zcaymaz <zbyrcmyz@gmail.com>
2023-05-10 16:24:53 +0300

    .

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.use(fileUpload({
 
 // Routes
 app.use('/user', require('./routes/userRouter'))
-app.use('/api', require('./routes/categoryRouter'))
-app.use('/api', require('./routes/upload'))
-app.use('/api', require('./routes/productRouter'))
-app.use('/api', require('./routes/paymentRouter'))
+// app.use('/api', require('./routes/categoryRouter'))
+// app.use('/api', require('./routes/upload'))
+// app.use('/api', require('./routes/productRouter'))
+// app.use('/api', require('./routes/paymentRouter'))
 app.use('/api', require('./routes/taskRouter'))
 
 
