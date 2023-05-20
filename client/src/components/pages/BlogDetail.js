930542ff37d8a038c98250673746af1a6c7b403a
mlhkcmn <melihkocaman06@hotmail.com>
2023-05-20 21:41:14 +0300

    Proje Detay Tamamlandı.

--- client/src/components/pages/BlogDetail.js
+++ client/src/components/pages/BlogDetail.js
@@ -1,6 +1,7 @@
 /* eslint-disable array-callback-return */
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import ImageGallery from 'react-image-gallery';
 import { Stack } from "@mui/material";
 import { formatDate } from "../common/FormatDate";
 
@@ -34,9 +35,9 @@ const BlogDetail = ({ match }) => {
   return (
     <div className="container mt-4 mb-4">
       <div className="row">
+        <ImageGallery flickThreshold={0.50} slideDuration={0} items={projectImages} showNav={true} showFullscreenButton={true} showPlayButton={false} />
         <div className="col-lg-8">
           <div className="blog-detail">
-            <img className="w-100" src="/img/product1.jpeg" alt="product" />
             <span className="blog-detail-category"></span>
             <h2 style={{ marginTop: "2rem" }} className="blog-detail-title">
               {project.title}
@@ -44,16 +45,17 @@ const BlogDetail = ({ match }) => {
             <p className="blog-detail-content" style={{ marginTop: "2.5rem" }}>
               {project.description}
             </p>
-            <h2 className="blog-detail-alttitle">Lorem ipsum dolor sit amet</h2>
-            <p className="blog-detail-content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum
-            </p>
+          </div>
+          <div className="fd-item fd-features">
+            <h4>Konut Özellikleri</h4>
+            <div className="row">
+              {projectFeatures.map((feature, index) => (
+                <div className="col-lg-6" key={index}>
+                  <i className="fa fa-check"></i>
+                  <span>{feature.title}</span>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
         <div className="col-lg-4">
@@ -62,7 +64,7 @@ const BlogDetail = ({ match }) => {
               <h4>Projeyi Envanterinde Tutan</h4>
               <div style={{ marginTop: "1rem" }} className="widget-content">
                 <ul className="category-ul">
-                  <li><i style={{paddingRight:'0.6rem'}} className="fas fa-store"/>{project.name}</li>
+                  <li><i style={{ paddingRight: '0.6rem' }} className="fas fa-store" />{project.name}</li>
                 </ul>
               </div>
             </div>
@@ -75,17 +77,7 @@ const BlogDetail = ({ match }) => {
                 </ul>
               </div>
             </div>
-            <div className="fd-item fd-features">
-              <h4>Konut Özellikleri</h4>
-              <div className="row">
-                {projectFeatures.map((feature, index) => (
-                  <div className="col-lg-6" key={index}>
-                    <i className="fa fa-check"></i>
-                    <span>{feature.title}</span>
-                  </div>
-                ))}
-              </div>
-            </div>
+
             <div className="widget">
               <h5>Proje Adresi</h5>
               <div style={{ marginTop: "1rem" }} className="widget-content">
